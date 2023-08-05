2225b1d01fc7b20ff900ff9e8dc25e1d6911e706
MrValioBg <valentin.v1102@gmail.com>
2023-08-05 13:17:16 +0300

    refactor: fix logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,7 +67,7 @@ const config = {
         title: 'Mazen Knowledgebase',
         logo: {
           alt: 'My Site Logo',
-          src: 'img/mazen-logo.svg',
+          src: 'img/logo.svg',
         },
         items: [
           {
